94d381f0aebcbd31b1d15071fc1b43499a85e2a9
Jaswanth-work <jaswanthmandali2006@gmail.com>
2026-04-27 16:04:12 +0530

    [Jaswanth] Added UC2-FEETANDINCHES

--- App/src/QuantityMeasurementApp.java
+++ App/src/QuantityMeasurementApp.java
@@ -16,9 +16,32 @@ public class QuantityMeasurementApp {
         }
     }
 
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches inches = (Inches) obj;
+            return Double.compare(this.value, inches.value) == 0;
+        }
+    }
+
+    public static boolean compareFeet(double a, double b) {
+        return new Feet(a).equals(new Feet(b));
+    }
+
+    public static boolean compareInches(double a, double b) {
+        return new Inches(a).equals(new Inches(b));
+    }
+
     public static void main(String[] args) {
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(1.0);
-        System.out.println(feet1.equals(feet2));
+        System.out.println(compareFeet(1.0, 1.0));
+        System.out.println(compareInches(1.0, 1.0));
     }
 }
